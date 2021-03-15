ee2674b1c14cccd00de697026f07047f159a8633
user <aax.chiri@gmail.com>
2021-03-15 09:17:23 +0900

    様々なバリデーションを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,25 @@
 class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  before_validation :add_lovely_to_cat
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  def add_lovely_to_cat
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
 end
